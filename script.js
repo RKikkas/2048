aae796d65e75153b8ededeee7631e1c65741108f
RKikkas <roger.kikkas@gmail.com>
2019-01-25 17:43:35 +0200

    Added function that adds a 2 or a 4 to a random empty spot on the grid.

--- script.js
+++ script.js
@@ -8,22 +8,43 @@ window.onkeyup = function(e) {
             arr.push(value);
         }
     }
-    console.log(arr);
 
-    let newArr = arr.reverse();
-
-    // ["16", "15", "14", "13", "12", "11", "10", "9", "8", "7", "6", "5", "4", "3", "2", "1"]
+    let newArr = [...arr];
 
     if (key === "w" || key === "W" || key === "ArrowUp"){
         console.log("up");
+        addToEmptySpot();
     } else if (key === "d" || key === "D" || key === "ArrowRight"){
         console.log("right");
+        addToEmptySpot();
     } else if (key === "s" || key === "S" || key === "ArrowDown"){
         console.log("down");
+        addToEmptySpot();
     } else if (key === "a" || key === "A" || key === "ArrowLeft"){
         console.log("left");
+        addToEmptySpot();
     }
 
+    // Checks empty slots on the grid, then assigns either a 2 or a 4 to a random empty slot
+    function addToEmptySpot(){
+        let emptySlots = [];
+        let emptySlotsNum = 1;
+        let randomSlot = 0;
+
+        for (let i = 0; i < arr.length; i++){
+            if (arr[i] === ""){
+                emptySlots.push([emptySlotsNum, i]);
+                emptySlotsNum++;
+            }
+        }
+
+        if (emptySlots.length) {
+            randomSlot = Math.floor(Math.random() * emptySlots.length);
+            newArr[emptySlots[randomSlot][1]] = Math.random() <= 0.5 ? "2" : "4";
+        }
+    }
+
+    // Inserts the new array values on to the grid
     for (let i = 1; i < 5; i++){
         let j = 1;
             for (let x = (i - 1) * 4; x < i * 4 ; x++){
@@ -31,6 +52,4 @@ window.onkeyup = function(e) {
                 j++;
             }
     }
-
-    // Function that adds new number to a random empty spot
 };
