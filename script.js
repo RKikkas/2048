1b02ca08220c02e7025395cf649490e1fd475251
RKikkas <roger.kikkas@gmail.com>
2019-02-01 12:17:23 +0200

    Moved the code that "plays" the numbers to a separate function.

--- script.js
+++ script.js
@@ -41,84 +41,7 @@ window.onkeyup = function(e) {
 
     if (key === "w" || key === "W" || key === "ArrowUp"){
 
-        // array for slots that have already been used this keypress
-        let usedSlots = [];
-
-        for (let i = 4; i < newArr.length; i++){
-
-            let value = newArr[i];
-            let upperValue = newArr[i - 4];
-            let upperValue2 = newArr[i - 8];
-            let upperValue3 = newArr[i - 12];
-            if (value !== ""){
-                if (upperValue === ""){
-                    // checks if its 3rd row
-                    if (upperValue2 !== undefined){
-                        // checks if its 4th row
-                        if (upperValue3 !== undefined){
-                            // 4th row
-                            if (upperValue2 === "" && upperValue3 === ""){
-                                newArr[i - 12] = value;
-                                newArr[i] = "";
-                            } else if (upperValue2 === "" && upperValue3){
-                                if (upperValue3 === value){
-                                    if (usedSlots.includes(i - 12)){
-                                        newArr[i - 8] = value;
-                                        newArr[i] = "";
-                                    } else {
-                                        newArr[i - 12] = (parseInt(value) + parseInt(upperValue3)).toString();
-                                        newArr[i] = "";
-                                        usedSlots.push(i - 12);
-                                    }
-                                } else {
-                                    newArr[i - 8] = value;
-                                    newArr[i] = "";
-                                }
-                            } else if (upperValue2 && upperValue3){
-                                if (upperValue2 === value){
-                                    if (usedSlots.includes(i - 8)){
-                                        newArr[i - 4] = value;
-                                        newArr[i] = "";
-                                    } else {
-                                        newArr[i - 8] = (parseInt(value) + parseInt(upperValue2)).toString();
-                                        newArr[i] = "";
-                                        usedSlots.push(i - 8);
-                                    }
-                                } else {
-                                    newArr[i - 4] = value;
-                                    newArr[i] = "";
-                                }
-                            }
-                        } // 3rd row
-                        else {
-                            if (upperValue2 === ""){
-                                newArr[i - 8] = value;
-                                newArr[i] = "";
-                            } else if (upperValue2 === value){
-                                if (usedSlots.includes(i - 8)){
-                                    newArr[i - 4] = value;
-                                    newArr[i] = "";
-                                } else {
-                                    newArr[i - 8] = (parseInt(value) + parseInt(upperValue2)).toString();
-                                    newArr[i] = "";
-                                    usedSlots.push(i - 8);
-                                }
-                            } else {
-                                newArr[i - 4] = value;
-                                newArr[i] = "";
-                            }
-                        }
-                    } else {
-                        newArr[i - 4] = value;
-                        newArr[i] = "";
-                    }
-                } else if (value === upperValue){
-                    newArr[i - 4] = (parseInt(value) + parseInt(upperValue)).toString();
-                    newArr[i] = "";
-                    usedSlots.push(i - 4);
-                }
-            }
-        }
+        playRound(newArr);
 
         console.log("up");
         if(!compare(arr, newArr)){
@@ -133,6 +56,86 @@ window.onkeyup = function(e) {
     } else if (key === "a" || key === "A" || key === "ArrowLeft"){
         console.log("left");
         addToEmptySpot();
+    }
+
+    function playRound(newArr){
+
+        // array for slots that have already been used this keypress
+        let usedSlots = [];
+
+        for (let i = 4; i < newArr.length; i++) {
+
+            let value = newArr[i];
+            let upperValue = newArr[i - 4];
+            let upperValue2 = newArr[i - 8];
+            let upperValue3 = newArr[i - 12];
+            if (value !== "") {
+                if (upperValue === "") {
+                    // checks if its 3rd row
+                    if (upperValue2 !== undefined) {
+                        // checks if its 4th row
+                        if (upperValue3 !== undefined) {
+                            // 4th row
+                            if (upperValue2 === "" && upperValue3 === "") {
+                                moveNumbers(i, 3, value, "move");
+                            } else if (upperValue2 === "" && upperValue3) {
+                                if (upperValue3 === value) {
+                                    if (usedSlots.includes(i - 12)) {
+                                        moveNumbers(i, 2, value, "move");
+                                    } else {
+                                        moveNumbers(i, 3, value, "add", upperValue3);
+                                    }
+                                } else {
+                                    moveNumbers(i, 2, value, "move");
+                                }
+                            } else if (upperValue2 && upperValue3) {
+                                if (upperValue2 === value) {
+                                    if (usedSlots.includes(i - 8)) {
+                                        moveNumbers(i, 1, value, "move");
+                                    } else {
+                                        moveNumbers(i, 2, value, "add", upperValue2);
+                                    }
+                                } else {
+                                    moveNumbers(i, 1, value, "move");
+                                }
+                            }
+                        } // 3rd row
+                        else {
+                            if (upperValue2 === "") {
+                                moveNumbers(i, 2, value, "move");
+                            } else if (upperValue2 === value) {
+                                if (usedSlots.includes(i - 8)) {
+                                    moveNumbers(i, 1, value, "move");
+                                } else {
+                                    moveNumbers(i, 2, value, "add", upperValue2);
+                                }
+                            } else {
+                                moveNumbers(i, 1, value, "move");
+                            }
+                        }
+                    } else {
+                        moveNumbers(i, 1, value, "move");
+                    }
+                } else if (value === upperValue) {
+                    moveNumbers(i, 1, value, "add", upperValue);
+                }
+            }
+        }
+
+        function moveNumbers(loopIndex, moveSlotsNum, value, useCase, value2) {
+            let arrPos = loopIndex - (moveSlotsNum * 4);
+            switch(useCase) {
+                case "move":
+                    newArr[arrPos] = value;
+                    newArr[loopIndex] = "";
+                    break;
+                case "add":
+                    newArr[arrPos] = (parseInt(value) + parseInt(value2)).toString();
+                    newArr[loopIndex] = "";
+                    usedSlots.push(arrPos);
+                    break;
+            }
+        }
     }
 
     // Checks empty slots on the grid, then assigns either a 2 or a 4 to a random empty slot
